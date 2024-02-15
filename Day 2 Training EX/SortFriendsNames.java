053d312b1eff769799523540ce853931fbd8b96c
dhiyaneshev <159748793+dhiyaneshev@users.noreply.github.com>
2024-02-15 12:11:28 +0530

    Create SortFriendsNames.java

---
+++ Day 2 Training EX/SortFriendsNames.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortFriendsNames {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[]{"Anirudh", "Ramesh", "Sridhar", "Suresh", "Zarina", "Anirudh", "Raju", "Mukesh"};
+
+        FriendsNames friendsNames = new FriendsNames(names);
+        friendsNames.sort();
+
+        for(String name: friendsNames.getNames()){
+            System.out.println(name);
+        }
+
+    }
+}
+
+class FriendsNames{
+    private String[] names;
+
+    public FriendsNames(String[] names) {
+        this.names = names;
+    }
+
+    protected void sort(){
+
+        for(int i=0;i<names.length-1;i++){
+            for(int j=i+1;j<names.length;j++){
+                if(names[i].charAt(0)>names[j].charAt(0)){
+                    String t = names[i];
+                    names[i] = names[j];
+                    names[j] = t;
+                }
+            }
+        }
+
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+}
